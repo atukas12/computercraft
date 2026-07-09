944ad68960b7dd4c26a97cadbf2b04a387d174c4
atukas12 <simulis.atas@gmail.com>
2026-07-09 18:43:52 +0300

    Initial upload

--- atas.lua
+++ atas.lua
@@ -1,2 +1,19 @@
-input = read("Your name? name: ")
-print("Hello "..input)
+--git add .
+--git commit -m "Initial upload"
+--git push
+
+code = "gaj12A"
+Secured = false
+
+print("Enter Pasword")
+while Secured == false do
+    input = read()
+
+    if input ~= code then
+        print("Wrong pasword try again")
+    else
+        Secured = true
+    end
+end
+
+print("Access granted")
